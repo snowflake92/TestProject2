bb0d7c05cd495ee264c6d029eb2d038ff4aaed43
Collin_K <vicariousdejavu@gmail.com>
2023-02-22 17:19:39 -0700

    initial commit

---
+++ Main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   Main.c
+ * Author: cklomp92
+ *
+ * Created on February 22, 2023, 5:06 PM
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TestProject2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TestProject2.X.production.hex
